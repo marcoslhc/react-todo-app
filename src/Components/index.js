a97354002e8a48cc58f662ae9a91d4aac75de8b9
Marcos Hernandez <marcoslhc@gmail.com>
2019-04-03 02:40:44 -0400

    Add missing files

---
+++ src/Components/index.js
@@ -0,0 +1,7 @@
+import CreateTaskButton from "./CreateTaskButton";
+import FilterBar from "./FilterBar";
+import FilterButton from "./FilterButton";
+import Task from "./Task";
+import TaskList from "./TaskList";
+
+export { CreateTaskButton, FilterBar, FilterButton, Task, TaskList };
